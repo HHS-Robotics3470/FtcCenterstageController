4108289310f0cd254e10fc650e3e0c2a9b96ae60
tinypapercrane <57554191+AdeelAmad@users.noreply.github.com>
2024-01-20 12:29:00 -0800

    drive constants changed and opencv thresholds and windows changed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpikeDetectionPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SpikeDetectionPipeline.java
@@ -12,14 +12,26 @@ public class SpikeDetectionPipeline extends OpenCvPipeline {
 
     int position = 0;
 
-    Point point_x_0 = new Point(0,100);
-    Point point_y_0 = new Point(90,200);
+    Point point_x_0 = new Point(100,50);
+    Point point_y_0 = new Point(200,150);
 
-    Point point_x_1 = new Point(475, 150);
-    Point point_y_1 = new Point(650, 250);
+    Point point_x_1 = new Point(625, 0);
+    Point point_y_1 = new Point(700, 100);
 
-    Point point_x_2 = new Point(1100, 100);
-    Point point_y_2 = new Point(1280, 200);
+    Point point_x_2 = new Point(1050, 50);
+    Point point_y_2 = new Point(1150, 150);
+
+    public double redValueSubmat0;
+    public double greenValueSubmat0;
+    public double blueValueSubmat0;
+
+    public double redValueSubmat1;
+    public double greenValueSubmat1;
+    public double blueValueSubmat1;
+
+    public double redValueSubmat2;
+    public double greenValueSubmat2;
+    public double blueValueSubmat2;
 
 
 
@@ -33,17 +45,17 @@ public class SpikeDetectionPipeline extends OpenCvPipeline {
         Scalar averageColorSubmat1 = Core.mean(submat1);
         Scalar averageColorSubmat2 = Core.mean(submat2);
 
-        double redValueSubmat0 = averageColorSubmat0.val[0];
-        double greenValueSubmat0 = averageColorSubmat0.val[1];
-        double blueValueSubmat0 = averageColorSubmat0.val[2];
+        redValueSubmat0 = averageColorSubmat0.val[0];
+        greenValueSubmat0 = averageColorSubmat0.val[1];
+        blueValueSubmat0 = averageColorSubmat0.val[2];
 
-        double redValueSubmat1 = averageColorSubmat1.val[0];
-        double greenValueSubmat1 = averageColorSubmat1.val[1];
-        double blueValueSubmat1 = averageColorSubmat1.val[2];
+        redValueSubmat1 = averageColorSubmat1.val[0];
+        greenValueSubmat1 = averageColorSubmat1.val[1];
+        blueValueSubmat1 = averageColorSubmat1.val[2];
 
-        double redValueSubmat2 = averageColorSubmat2.val[0];
-        double greenValueSubmat2 = averageColorSubmat2.val[1];
-        double blueValueSubmat2 = averageColorSubmat2.val[2];
+        redValueSubmat2 = averageColorSubmat2.val[0];
+        greenValueSubmat2 = averageColorSubmat2.val[1];
+        blueValueSubmat2 = averageColorSubmat2.val[2];
 
         boolean isSubmat0Red = (redValueSubmat0 > greenValueSubmat0 && redValueSubmat0 > blueValueSubmat0);
         boolean isSubmat0Blue = (blueValueSubmat0 > redValueSubmat0 && blueValueSubmat0 > greenValueSubmat0);
@@ -54,19 +66,19 @@ public class SpikeDetectionPipeline extends OpenCvPipeline {
         boolean isSubmat2Red = (redValueSubmat2 > greenValueSubmat2 && redValueSubmat2 > blueValueSubmat2);
         boolean isSubmat2Blue = (blueValueSubmat2 > redValueSubmat2 && blueValueSubmat2 > greenValueSubmat2);
 
-        if ((isSubmat1Blue || isSubmat1Red) && (redValueSubmat1 > 75 || blueValueSubmat1 > 75)) {
+        if ((isSubmat1Blue || isSubmat1Red) && (redValueSubmat1 > 200 || blueValueSubmat1 > 200)) {
             position = 1;
-        } else if ((isSubmat0Blue || isSubmat0Red) && (redValueSubmat0 > 75 || blueValueSubmat0 > 75)){
+        } else if ((isSubmat0Blue || isSubmat0Red) && (redValueSubmat0 > 200 || blueValueSubmat0 > 200)){
             position = 0;
-        }else if ((isSubmat2Blue || isSubmat2Red) && (redValueSubmat2 > 75 || blueValueSubmat2 > 75)) {
+        }else if ((isSubmat2Blue || isSubmat2Red) && (redValueSubmat2 > 200 || blueValueSubmat2 > 200)) {
             position = 2;
         } else {
-            position = 0;
+            position = 3;
         }
 
-        Imgproc.rectangle(input, point_x_1, point_y_1, averageColorSubmat1, 2);
-        Imgproc.rectangle(input, point_x_0, point_y_0, averageColorSubmat0, 2);
-        Imgproc.rectangle(input, point_x_2, point_y_2, averageColorSubmat2, 2);
+        Imgproc.rectangle(input, point_x_1, point_y_1, new Scalar(0, 0, 0), 2);
+        Imgproc.rectangle(input, point_x_0, point_y_0, new Scalar(0, 0, 0), 2);
+        Imgproc.rectangle(input, point_x_2, point_y_2, new Scalar(0, 0, 0), 2);
 
         submat1.release();
         submat2.release();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AdeelAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AdeelAuto.java
@@ -22,6 +22,18 @@ public class AdeelAuto extends LinearOpMode {
         waitForStart();
         while (opModeIsActive()) {
             telemetry.addData("Position:", pipeline.getSpikePosition());
+
+            telemetry.addData("Red submat 0:", pipeline.redValueSubmat0);
+            telemetry.addData("Green submat 0:", pipeline.greenValueSubmat0);
+            telemetry.addData("Blue submat 0:", pipeline.blueValueSubmat0);
+
+            telemetry.addData("Red submat 1:", pipeline.redValueSubmat1);
+            telemetry.addData("Green submat 1:", pipeline.greenValueSubmat1);
+            telemetry.addData("Blue submat 1:", pipeline.blueValueSubmat1);
+
+            telemetry.addData("Red submat 2:", pipeline.redValueSubmat2);
+            telemetry.addData("Green submat 2:", pipeline.greenValueSubmat2);
+            telemetry.addData("Blue submat 2:", pipeline.blueValueSubmat2);
             telemetry.update();
         }
     }
