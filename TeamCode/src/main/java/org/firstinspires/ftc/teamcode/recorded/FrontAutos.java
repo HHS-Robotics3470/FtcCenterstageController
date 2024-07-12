ade19537bd9ee7dd3887d0569bf719a03e98181e
yukon1 <tsean.susanto@gmail.com>
2024-07-11 17:19:13 -0700

    Finished auto Added Auto Tweaks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/recorded/FrontAutos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/recorded/FrontAutos.java
@@ -10,7 +10,119 @@ public class FrontAutos extends RunAutos {
         if (ifMirror)
             n *= -1;
         robot.resetEncoders();
-
+        //move to spike marks (strafe)
+        for (int helpme = 0; helpme < 100; helpme++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, -380, 380, 380, -380, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+        }
+        robot.resetEncoders();
+        //move to spike marks (forward)
+        for (int adithya = 0; adithya < 100; adithya++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, 720, 720, 720, 720, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+        }
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        robot.resetEncoders();
+        //back up from pixel
+        for (int christian = 0; christian < 100; christian++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, -150, -150, -150, -150, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+        }
+        robot.resetEncoders();
+        //strafe to board
+        for (int katti = 0; katti < 100; katti++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, 1000, -1000, -1000, 1000, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+        }
+        robot.resetEncoders();
+        //strafe to board
+        for (int katti = 0; katti < 100; katti++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, 500, -500, -500, 500, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+        }
+        robot.resetEncoders();
+        //rotate 90
+        for (int gavin = 0; gavin < 100; gavin++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, -720, 720, -720, 720, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+        }
+        if(!ifMirror) {
+            robot.resetEncoders();
+            //strafe to orient to pos
+            for (int poop = 0; poop < 100; poop++) {
+                robot.setMovementPosition(0.0, 0.0, 0.0, -545, 545, 545, -545, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+            }
+        }
+        if(ifMirror){
+            robot.resetEncoders();
+            //strafe to orient to pos
+            for (int poop = 0; poop < 100; poop++) {
+                robot.setMovementPosition(0.0, 0.0, 0.0, -520, 520, 520, -520, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+            }
+        }
+        if (!ifMirror) {
+            robot.resetEncoders();
+            //move towards board
+            for (int chanelle = 0; chanelle < 100; chanelle++) {
+                robot.setMovementPosition(0.0, 0.0, 0.0, 175, 175, 175, 175, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+            }
+        }
+        if(ifMirror){
+            robot.resetEncoders();
+            //move towards board
+            for (int chanelle = 0; chanelle < 100; chanelle++) {
+                robot.setMovementPosition(0.0, 0.0, 0.0, 275, 275, 275, 275, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+            }
+        }
+        robot.resetEncoders();
+        //raise lift
+        for (int lucas = 0; lucas < 100; lucas++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, 0, 0, 0, 0, -1600, -1600, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        //swing arm out
+        for (int areeb = 0; areeb < 100; areeb++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, 0, 0, 0, 0, -1600, -1600, false, 0.95, 0.34, 0.049, 0.1, 0.11, false, ifMirror);
+        }
+        //lower lift slightly
+        for (int me = 0; me < 100; me++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, 0, 0, 0, 0, -1100, -1100, false, 0.95, 0.34, 0.049, 0.1, 0.11, false, ifMirror);
+        }
+        //move towards board
+        for (int adeel = 0; adeel < 100; adeel++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, 225, 225, 225, 225, -1100, -1100, false, 0.95, 0.34, 0.049, 0.1, 0.11, false, ifMirror);
+        }
+        //Open Claw
+        for (int asd = 0; asd < 100; asd++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, 225, 225, 225, 225, -1100, -1100, false, 0.95, 0.34, 0.0315, 0.1, 0.11, false, ifMirror);
+        }
+        //Close Claw
+        for (int pardi = 0; pardi < 100; pardi++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, 225, 225, 225, 225, -1100, -1100, false, 0.95, 0.34, 0.049, 0.1, 0.11, false, ifMirror);
+        }
+        //back up
+        for (int sraditya = 0; sraditya < 100; sraditya++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, 100, 100, 100, 100, -1100, -1100, false, 0.95, 0.34, 0.049, 0.1, 0.11, false, ifMirror);
+        }
+        //reraise lift
+        for (int asda = 0; asda < 100; asda++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, 100, 100, 100, 100, -1600, -1600, false, 0.95, 0.34, 0.049, 0.1, 0.11, false, ifMirror);
+        }
+        //reset arm
+        for (int parthiv = 0; parthiv < 100; parthiv++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, 100, 100, 100, 100, -1700, -1700, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+        }
+        //lower lift
+        for (int areebyoung = 0; areebyoung < 100; areebyoung++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, 100, 100, 100, 100, 200, 200, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+        }
+        robot.resetEncoders();
+        //strafe to park
+        for (int lol = 0; lol < 100; lol++) {
+            robot.setMovementPosition(0.0, 0.0, 0.0, 700, -750, -750, 750, 300, 300, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+        }
     }
 
     public void run_1(robotHardware robot, boolean ifMirror) {
@@ -136,15 +248,33 @@ public class FrontAutos extends RunAutos {
         for (int gavin = 0; gavin < 100; gavin++) {
             robot.setMovementPosition(0.0, 0.0, 0.0, -720, 720, -720, 720, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
         }
-        robot.resetEncoders();
-        //strafe to orient to pos
-        for (int poop = 0; poop < 100; poop++) {
-            robot.setMovementPosition(0.0, 0.0, 0.0, -150, 150, 150, -150, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+        if(!ifMirror) {
+            robot.resetEncoders();
+            //strafe to orient to pos
+            for (int poop = 0; poop < 100; poop++) {
+                robot.setMovementPosition(0.0, 0.0, 0.0, -150, 150, 150, -150, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+            }
         }
-        robot.resetEncoders();
-        //move towards board
-        for (int chanelle = 0; chanelle < 100; chanelle++) {
-            robot.setMovementPosition(0.0, 0.0, 0.0, 75, 75, 75, 75, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+        if(ifMirror){
+            robot.resetEncoders();
+            //strafe to orient to pos
+            for (int poop = 0; poop < 100; poop++) {
+                robot.setMovementPosition(0.0, 0.0, 0.0, -110, 110, 110, -110, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+            }
+        }
+        if(!ifMirror) {
+            robot.resetEncoders();
+            //move towards board
+            for (int chanelle = 0; chanelle < 100; chanelle++) {
+                robot.setMovementPosition(0.0, 0.0, 0.0, 75, 75, 75, 75, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+            }
+        }
+        if(ifMirror){
+            robot.resetEncoders();
+            //move towards board
+            for (int chanelle = 0; chanelle < 100; chanelle++) {
+                robot.setMovementPosition(0.0, 0.0, 0.0, 175, 175, 175, 175, 0, 0, false, 0.02, 0.47, 0.049, 0.1, 0.11, false, ifMirror);
+            }
         }
         robot.resetEncoders();
         //raise lift
